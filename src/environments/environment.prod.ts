686bf4813f9540d356afdc2174ea7eb68ba097c6
Arseniy A <arseniyandru@gmail.com>
2020-12-07 21:33:01 -0800

    wip: update prod grid url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gridUrl: 'http://mockbin.org/bin/8cd40d4a-50dd-4239-9449-9c9f34796914'
+  gridUrl: 'https://mockbin.org/bin/8cd40d4a-50dd-4239-9449-9c9f34796914'
 };
