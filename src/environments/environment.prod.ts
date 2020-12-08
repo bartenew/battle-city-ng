5a9a75824378c704a8d5f14b8de0cae195b9a42b
Arseniy A <arseniyandru@gmail.com>
2020-12-07 21:38:19 -0800

    wip: update prod grid url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gridUrl: 'https://mockbin.org/bin/8cd40d4a-50dd-4239-9449-9c9f34796914'
+  gridUrl: 'https://battle-city-ng.free.beeceptor.com/grid.json'
 };
